b1cf1276c874a928e495e18864ed79f4c2ab1e19
Cloie Park <greatwisd@gmail.com>
2019-02-27 14:54:57 +0900

    routing and modify DB schema

--- app/app.js
+++ app/app.js
@@ -3,10 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var mapscreenRouter = require('./routes/mapcsreen');
+var indexRouter = require('./routes/indexRouter');
+var usersRouter = require('./routes/usersRouter');
+var searchRouter = require('./routes/searchRouter');
 
 var app = express();
 const port = 3001;
@@ -15,6 +14,8 @@ const port = 3001;
 // app.set("view engine", "pug");
 const models = require('./models/index');
 
+
+// Check DB-Server Connetion
 models.sequelize
     .sync()
     .then(() => {
@@ -31,16 +32,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routing
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mapscreen', mapscreenRouter);
+app.use('/search', searchRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
     next(createError(404));
 });
-// error handler
-app.use(function(err, req, res, next) {
+
+// Error handler
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
