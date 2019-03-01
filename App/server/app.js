14d8a098ed0c7bfa989c20f7f80def3468d326ad
JEONGJIHUN <lippin21@gmail.com>
2019-03-01 16:20:18 +0900

    Changed directory

--- App/server/app.js
+++ App/server/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const indexRouter = require('./routes/indexRouter');
 const userRouter = require('./routes/userRouter');
 const searchRouter = require('./routes/searchRouter');
-const category = require('../app/categories');
+const category = require('./categories');
 
 const app = express();
 const port = 3001;
@@ -45,12 +45,12 @@ app.use('/search', searchRouter);
 // });
 
 // Catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     next(createError(404));
 });
 
 // Error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
