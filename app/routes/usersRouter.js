d783732ee20cc5014774475c77089351218fd365
Cloie Park <greatwisd@gmail.com>
2019-03-01 14:47:29 +0900

    modify userRouter

--- app/routes/usersRouter.js
+++ app/routes/usersRouter.js
@@ -6,20 +6,26 @@ var Drawing = require('../models').drawing;
 
 router.get('/', async (req, res, next) => {
     const { name } = req.body;
-    await User.create({ name });
-    res.status(200).send('유저 이름을 Row로 저장했습니다! :)');
+    try {
+        await User.findOrCreate({ name });
+        res.status(200).send('유저 이름을 Row로 저장했습니다! :)');
+    } catch {
+        res.status(500).send('이름 저장에 실패했습니다.');
+    }
 });
 
 router.post('/save', async (req, res, next) => {
     const factorId = req.body.factor_id;
     const isValidFactorId = !!(factorId >= 1 || factorId <= 6);
-
-    if (isValidFactorId) {
-        await Drawing.create(req.body);
-        res.status(200).send('성공적으로 호재 정보를 저장했습니다! :)');
+    try {
+        if (isValidFactorId) {
+            await Drawing.create(req.body);
+            res.status(200).send('성공적으로 호재 정보를 저장했습니다! :)');
+        }
+    } catch {
+        res.status(400).send('데이터 저장에 실패했습니다.');
     }
 
-    res.status(400).send('데이터 저장에 실패했습니다.');
 });
 
 module.exports = router;
