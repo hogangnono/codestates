152e121191c6524d70bc7e601e046d15544d4c4a
DoheeKang <doheee.kang@gmail.com>
2019-03-01 09:44:37 +0900

    Modify whole file with eslint

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
+/* eslint-disable linebreak-style */
 import React, { Component } from 'react';
-import Toolbox from './Toolbox';
 import * as d3 from 'd3';
-import CustomOverlay from './CustomOverlay'
-import './App.css'
-
+import Toolbox from './Toolbox';
+import CustomOverlay from './CustomOverlay';
+import './App.css';
 class App extends Component {
-  state = {
-    map: undefined
-  }
-  componentDidMount() {
-    let naver = window.naver;
-    let mapOptions = {
-      zoomControl: true,
-      zoomControlOptions: {
-        style: naver.maps.ZoomControlStyle.SMALL,
-        position: naver.maps.Position.TOP_RIGHT
-      },
-      logoControl: true,
-      logoControlOptions: {
-        position: naver.maps.Position.BOTTOM_RIGHT
-      },
-      scaleControl: true,
-      scaleControlOptions: {
-        position: naver.maps.Position.BOTTOM_RIGHT
-      },
-      mapDataControl: true,
-      mapDataControlOptions: {
-        position: naver.maps.Position.BOTTOM_LEFT
-      }
+    state = {
+        map: undefined
     }
-    let map = new naver.maps.Map(d3.select('#map').node(), mapOptions);
-    this.setState({ map })
 
-    naver.maps.Event.addListener(map, 'click', function (e) { //클릭한 위치에 오버레이를 추가합니다.
-      console.log('click');
-      new CustomOverlay({
-        position: e.coord
-      }).setMap(map);
-    });
-  }
-  render() {
-    return (
-      <div id='wrapper'>
-        <div id='map' style={{ height: '100vh' }}></div>
-        <Toolbox map={this.state.map}></Toolbox>
-      </div>
-    );
-  }
+    componentDidMount() {
+        const naver = window.naver;
+        const mapOptions = {
+            zoomControl: true,
+            zoomControlOptions: {
+                style: naver.maps.ZoomControlStyle.SMALL,
+                position: naver.maps.Position.TOP_RIGHT
+            },
+            logoControl: true,
+            logoControlOptions: {
+                position: naver.maps.Position.BOTTOM_RIGHT
+            },
+            scaleControl: true,
+            scaleControlOptions: {
+                position: naver.maps.Position.BOTTOM_RIGHT
+            },
+            mapDataControl: true,
+            mapDataControlOptions: {
+                position: naver.maps.Position.BOTTOM_LEFT
+            }
+        };
+        const map = new naver.maps.Map(d3.select('#map').node(), mapOptions);
+        this.setState({ map });
+        naver.maps.Event.addListener(map, 'click', function (e) { // 클릭한 위치에 오버레이를 추가합니다.
+            console.log(map.getZoom());
+            new CustomOverlay({
+                position: e.coord
+            }).setMap(map);
+        });
+    }
+
+    render() {
+        const { map } = this.state;
+        return (
+            <div id="wrapper">
+                <div id="map" style={{ height: '100vh' }}></div>
+                <Toolbox map={map}></Toolbox>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/CustomOverlay.js
+++ src/CustomOverlay.js
@@ -1,11 +1,12 @@
-import * as d3 from 'd3'
-
+/* eslint-disable linebreak-style */
+import * as d3 from 'd3';
 var CustomOverlay = function (options) {
-    //make a div that contain shape
-    let div = document.createElement("div")
-    let svg = d3.create('svg')
-    svg.append('circle').attr('cx', 50).attr('cy', 50).attr("r", 50).attr("stroke", "green").attr("fill", "green").attr("fill-opacity", "0.4")
-    div.appendChild(svg.node())
+    // eslint-disable-next-line linebreak-style
+    // make a div that contain shape
+    const div = document.createElement('div');
+    const svg = d3.create('svg');
+    svg.append('circle').attr('cx', 50).attr('cy', 50).attr('r', 50).attr('stroke', 'green').attr('fill', 'green').attr('fill-opacity', '0.4');
+    div.appendChild(svg.node());
 
     this._element = div;
     this.setPosition(options.position);
@@ -35,11 +36,11 @@ CustomOverlay.prototype.draw = function () {
     if (!this.getMap()) {
         return;
     }
-    var projection = this.getProjection(),
-        position = this.getPosition(),
-        pixelPosition = projection.fromCoordToOffset(position);
-    console.log(pixelPosition)
-    this._element.style.position = "absolute";
+    const projection = this.getProjection();
+    const position = this.getPosition();
+    const pixelPosition = projection.fromCoordToOffset(position);
+    console.log(pixelPosition);
+    this._element.style.position = 'absolute';
 
     this._element.style.left = `${pixelPosition.x}px`;
     this._element.style.top = `${pixelPosition.y}px`;
