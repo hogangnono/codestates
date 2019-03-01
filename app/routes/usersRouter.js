bdf1275e30ea0e201df7bcd581733594a88e8700
Cloie Park <greatwisd@gmail.com>
2019-03-01 14:05:22 +0900

    modify usersRouter for /user and /users/save

--- app/routes/usersRouter.js
+++ app/routes/usersRouter.js
@@ -1,70 +1,25 @@
 var express = require('express');
 var router = express.Router();
-// var User = require('../models').user;
-// var Drawings = require('../models').drawing;
+var User = require('../models').user;
+var Drawing = require('../models').drawing;
 // var FactorCategories = require('../models').factor;
 
-router.get('/', (req, res, next) => {
-    res.status(200);
-    res.send('Welcome to hojae hojae!\nthis is a user page');
+router.get('/', async (req, res, next) => {
+    const { name } = req.body;
+    await User.create({ name });
+    res.status(200).send('유저 이름을 Row로 저장했습니다! :)');
 });
 
-// router.post('/', (req, res) => {
-//     const { name } = req.body;
-//     console.log('name', name);
-//     User
-//         .findOrCreate({ where: { name: name } })
-//         .spread((user, created) => {
-//             if (created) {
-//                 console.log('========= RESULT ===========');
-//                 console.log('POST /users/ :: DB에 없는 데이터여서 저장했습니다.');
-//                 console.log('============================');
-//                 res.status(201).send('DB에 없는 데이터여서 저장했습니다.');
-//             } else {
-//                 console.log('========= RESULT ===========');
-//                 console.log('POST /users/ :: user information :\n', user.dataValues);
-//                 console.log('============================');
-//                 res.status(200).send('DB에 이미 있는 데이터입니다. 로그인만 했습니다.');
-//             }
-//         })
-//         .catch(err => {
-//             console.log('========= RESULT ===========');
-//             console.log('POST /users/ :: request was failed beacuse :\n', err);
-//             console.log('============================');
-//             res.status(500).send('POST /users/ request was failed');
-//         });
-// });
+router.post('/save', async (req, res, next) => {
+    const factorId = req.body.factor_id;
+    const isValidFactorId = !!(factorId >= 1 || factorId <= 6);
 
-// router.post('/save', async (req, res) => {
-//     const { name, figureInfo, drawingInfo } = req.body;
-//     const { factorCategoryId } = figureInfo;
-//     const isValidfactorCategoryId = !!(factorCategoryId >= 1 && factorCategoryId <= 9);
+    if (isValidFactorId) {
+        await Drawing.create(req.body);
+        res.status(200).send('성공적으로 호재 정보를 저장했습니다! :)');
+    }
 
-//     try {
-//         if (isValidfactorCategoryId) {
-//             const findUser = await User.findOne({ where: { name: name } });
-//             const userID = await findUser.dataValues.id;
-//             const createdRowInFigures = await Figures.create(figureInfo);
-//             const figureId = await createdRowInFigures.dataValues.id;
-
-//             Drawings.create({
-//                 mapCenterLat: drawingInfo.mapCenterLat,
-//                 mapCenterLng: drawingInfo.mapCenterLng,
-//                 userID: userID,
-//                 figureId: figureId
-//             });
-//             console.log('========= RESULT ===========');
-//             console.log('POST /save/ :: 저장을 완료했습니다');
-//             console.log('============================');
-//             res.status(201).send('Figures에 데이터를 추가했습니다.');
-//         }
-//     } catch (err) {
-//         console.log('========= RESULT ===========');
-//         console.log('POST /save/ :: request was failed beacuse : \n', err);
-//         console.log('============================');
-//         res.status(400).send('데이터 등록 실패! 이유는 console을 확인해야해요!');
-//     }
-
-// });
+    res.status(400).send('데이터 저장에 실패했습니다.');
+});
 
 module.exports = router;
