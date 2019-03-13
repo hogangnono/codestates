065b0c3ec5d0a3a69b1d1722bd887748069cf620
JEONGJIHUN <lippin21@gmail.com>
2019-03-13 21:58:33 +0900

    Deleted useless code

--- packages/client/src/postSlackApi.js
+++ packages/client/src/postSlackApi.js
@@ -8,26 +8,11 @@ export const slackApi = (name, text = '호재 알림입니다.') => {
     });
     const options = {
         mrkdwn: true,
+        // type: 'interactive_message',
         attachments: [
             {
                 title: `${name}님의 호재 정보입니다.`,
                 color: '#4d55b2'
-                // fields: [
-                //     {
-                //         title: 'Volume',
-                //         value: '1',
-                //         short: true
-                //     },
-                //     {
-                //         title: 'Issue',
-                //         value: '3',
-                //         short: true
-                //     }
-                // ],
-                // author_name: `${name}`
-                // author_icon:
-                //     'http://a.slack-edge.com/7f18https://a.slack-edge.com/a8304/img/api/homepage_custom_integrations-2x.png',
-                // image_url: 'http://i.imgur.com/OJkaVOI.jpg?1'
             },
             {
                 title: 'Description',
@@ -37,10 +22,9 @@ export const slackApi = (name, text = '호재 알림입니다.') => {
             {
                 fallback: '호재 정보를 database 에 저장하시겠습니까?',
                 title: '호재 정보를 database 에 저장하시겠습니까??',
-                // callback_id: 'comic_1234_xyz',
-                // color: '#3AA3E3',
                 color: '#4d55b2',
                 attachment_type: 'default',
+                callback_id: 'hogangnono_callback_id',
                 actions: [
                     {
                         name: 'Accept',
@@ -61,8 +45,7 @@ export const slackApi = (name, text = '호재 알림입니다.') => {
                         type: 'button',
                         value: 'Refuse',
                         confirm: {
-                            title: 'Are you sure?',
-                            text: '정말 거절하시겠습니까?',
+                            title: '정말 거절하시겠습니까?',
                             ok_text: 'Yes',
                             dismiss_text: 'No'
                         }
