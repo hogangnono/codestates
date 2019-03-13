78d057f81656f4a7494b8fc59681ef1ade8bc490
JEONGJIHUN <lippin21@gmail.com>
2019-03-13 16:24:02 +0900

    export post slack api function and added slack api path in constants.js

--- packages/client/src/Components/Drawing.js
+++ packages/client/src/Components/Drawing.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import '../less/Drawing.less';
-import axios from 'axios';
 import Button from '../Module/Button';
 import Line from '../CustomOverlay/Line';
 import Arrow from '../CustomOverlay/Arrow';
@@ -10,6 +9,7 @@ import Rect from '../CustomOverlay/Rect';
 import Polygon from '../CustomOverlay/Polygon';
 import MyDrawingElement from './MyDrawingElement';
 import saveAction from '../Module/saveAction';
+import * as post from '../postSlackApi';
 
 class Drawing extends Component {
     static propTypes = {
@@ -164,90 +164,6 @@ class Drawing extends Component {
         this.setState({ refresh: !refresh });
     };
 
-    postSlackApi = (name, text = '호재 알림입니다.') => {
-        const a = [];
-        text.forEach(el => {
-            a.push(el.description);
-        });
-        const options = {
-            mrkdwn: true,
-            attachments: [
-                {
-                    title: `${name}님의 호재 정보입니다.`,
-                    color: '#4d55b2'
-                    // fields: [
-                    //     {
-                    //         title: 'Volume',
-                    //         value: '1',
-                    //         short: true
-                    //     },
-                    //     {
-                    //         title: 'Issue',
-                    //         value: '3',
-                    //         short: true
-                    //     }
-                    // ],
-                    // author_name: `${name}`
-                    // author_icon:
-                    //     'http://a.slack-edge.com/7f18https://a.slack-edge.com/a8304/img/api/homepage_custom_integrations-2x.png',
-                    // image_url: 'http://i.imgur.com/OJkaVOI.jpg?1'
-                },
-                {
-                    title: 'Description',
-                    text: `${a[0]}`,
-                    color: '#4d55b2'
-                },
-                {
-                    fallback: '호재 정보를 database 에 저장하시겠습니까?',
-                    title: '호재 정보를 database 에 저장하시겠습니까??',
-                    // callback_id: 'comic_1234_xyz',
-                    // color: '#3AA3E3',
-                    color: '#4d55b2',
-                    attachment_type: 'default',
-                    actions: [
-                        {
-                            name: 'Accept',
-                            text: 'Accept',
-                            type: 'button',
-                            value: 'Accept'
-                        },
-                        {
-                            name: 'Hold',
-                            text: 'Hold',
-                            type: 'button',
-                            value: 'Hold'
-                        },
-                        {
-                            name: 'Refuse',
-                            text: 'Refuse',
-                            style: 'danger',
-                            type: 'button',
-                            value: 'Refuse',
-                            confirm: {
-                                title: 'Are you sure?',
-                                text: '정말 거절하시겠습니까?',
-                                ok_text: 'Yes',
-                                dismiss_text: 'No'
-                            }
-                        }
-                    ]
-                }
-            ]
-        };
-
-        axios
-            .post(
-                'https://hooks.slack.com/services/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',
-                JSON.stringify(options)
-            )
-            .then(response => {
-                console.log('SUCCEEDED: Sent slack webhook: \n', response.data);
-            })
-            .catch(error => {
-                console.log('FAILED: Send slack webhook', error);
-            });
-    };
-
     render() {
         const {
             map,
@@ -278,15 +194,6 @@ class Drawing extends Component {
                 })}
                 <div id="myDrawingsContainer">
                     <MyDrawingElement drawingData={drawingData} />
-                    {/* {drawingData.map((shape, index) => {
-                        const newIndex = index + 1;
-                        return (
-                            <MyDrawingElement
-                                key={'Idrew' + newIndex}
-                                drawingData={drawingData}
-                            />
-                        );
-                    })} */}
                 </div>
                 <div id="saveCloseBtns">
                     <button
@@ -294,7 +201,7 @@ class Drawing extends Component {
                         className="saveCloseBtn"
                         onClick={() => {
                             // this.handleRequestSave('/user/save', drawingData);
-                            this.postSlackApi('hogangnono', NearByFactorItems);
+                            post.slackApi('hogangnono', NearByFactorItems);
                         }}
                     >
                         {`저장`}
